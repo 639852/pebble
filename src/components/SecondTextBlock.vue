f1210e464bafa050cd2f376885ed7cda0e2ffae0
Alexander Bobylev <sasha.bobyliev@mail.ru>
2026-06-24 14:35:55 +0400

    feat: added second text block

--- src/components/SecondTextBlock.vue
+++ src/components/SecondTextBlock.vue
@@ -1,11 +1,57 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { computed, inject, onMounted, useTemplateRef } from 'vue'
+import { splitString } from '@/helpers'
+
+import type { ComputedRef } from 'vue'
+
+const textEl = useTemplateRef('textEl')
+const pageScrollY = inject<ComputedRef<number>>('pageScrollY')
+
+const scrollTextProgress = computed(() => {
+  const textHeight = textEl.value?.offsetHeight ?? 0
+
+  const y = Math.max(pageScrollY?.value ?? 0, 0)
+  const scrollProgress = y / textHeight
+
+  return scrollProgress - 4.9
+})
+
+onMounted(() => {
+  if (textEl.value) splitString(textEl.value)
+})
+</script>
 
 <template>
-  <div class="second-text"></div>
+  <div
+    ref="textEl"
+    class="second-text"
+  >
+    <p class="second-text__text">What others are saying about Pebble</p>
+  </div>
 </template>
 
 <style scoped lang="scss">
 .second-text {
-  height: 19.3svh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  height: 100svh;
+  text-align: center;
+
+  &__text {
+    max-width: 100rem;
+
+    font-family: 'Youth', sans-serif;
+    @include mix.text-style(10.6rem);
+    line-height: 1;
+    letter-spacing: -0.5rem;
+
+    transform: translateY(min(0.001px, -100px + v-bind('scrollTextProgress') * 200px));
+
+    & > :deep(span) {
+      opacity: max(0.4, v-bind('scrollTextProgress') / (var(--char-index) / var(--text-length)));
+    }
+  }
 }
 </style>
